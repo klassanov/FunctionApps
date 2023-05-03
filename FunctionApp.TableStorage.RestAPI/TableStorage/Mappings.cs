6c33c7cb9ae1509a6e9420813b49f65eb8545499
Alexander Klassanov <klassanov@live.com>
2023-05-03 22:32:45 +0300

    Using a table storage for API

---
+++ FunctionApp.TableStorage.RestAPI/TableStorage/Mappings.cs
@@ -0,0 +1,22 @@
+﻿namespace FunctionApp.TableStorage.RestAPI.TableStorage
+{
+    public static class Mappings
+    {
+        public static ToDoTableEntity ToTableEntity(this Todo todo)
+        {
+            return new ToDoTableEntity()
+            {
+                PartitionKey = Constants.ToDoPartitionKey,
+                RowKey = todo.Id,
+                CreatedTime = todo.CreatedTime,
+                Description = todo.TaskDescription,
+                IsCompleted = todo.IsCompleted,
+            };
+        }
+
+        public static Todo ToTodo(this ToDoTableEntity entity)
+        {
+            return new Todo(entity.RowKey, entity.CreatedTime, entity.Description, entity.IsCompleted);
+        }
+    }
+}
---
+++ FunctionApp.TableStorage.RestAPI/TableStorage/ToDoTableEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace FunctionApp.TableStorage.RestAPI.TableStorage
+{
+    // Need to inherit TableEntity
+    public class ToDoTableEntity : TableEntity
+    {
+        public DateTime CreatedTime { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
