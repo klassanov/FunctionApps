9179845e168a600c43cb69aa847dcc5fdef8d89f
Alexander Klassanov <klassanov@live.com>
2023-05-11 11:11:03 +0300

    Writing to the blob storage

--- FunctionApp.Queue.RestAPI/QueueListener.cs
+++ FunctionApp.Queue.RestAPI/QueueListener.cs
@@ -1,3 +1,8 @@
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 
@@ -5,16 +10,32 @@ namespace FunctionApp.Queue.RestAPI
 {
     public class QueueListener
     {
+        //It is also possible to use a Strean directly in the binding
         [FunctionName("QueueListener")]
-        public void Run(
-            //We get the JSON serialized item
+        public async Task Run(
+
+            //This way We get the JSON serialized item, so we will need to deserialize it
             //[QueueTrigger(queueName: "todos", Connection = "AzureWebJobStorage")] string myQueueItem,
 
-            //We ask Azure functions to deserialize the item for us
-            [QueueTrigger(queueName: "todos", Connection = "AzureWebJobStorage")] Todo todo,
+            //This way We ask Azure functions to deserialize the item for us
+            [QueueTrigger(queueName: "todos", Connection = "AzureWebJobsStorage")] Todo todo,
+            [Blob(blobPath: "todos", Connection = "AzureWebJobsStorage")] BlobContainerClient containerClient,
             ILogger log)
         {
-            //log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+            containerClient.CreateIfNotExists();
+            var unicodeEncoding = new UnicodeEncoding();
+            var message = $"Created a new task: {todo.TaskDescription}";
+            var bytes = unicodeEncoding.GetBytes(message);
+            var client = containerClient.GetBlobClient($"{todo.Id}.txt");
+
+            using (var blobStream = new MemoryStream())
+            {
+                await blobStream.WriteAsync(bytes);
+                blobStream.Position = 0;
+                await client.UploadAsync(blobStream);
+            }
+                       
+            log.LogInformation($"C# Queue trigger function processed: {todo.TaskDescription}");
         }
     }
 }
